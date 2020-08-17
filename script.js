ff7a70eef5227aeebbb6f1f586ee6c6bcbfbf9d9
Haz-git <zhouharry1@gmail.com>
2020-08-17 09:27:47 -0700

    Updated Income and Expense Upon History Deletion

--- script.js
+++ script.js
@@ -64,6 +64,14 @@ function delChild(parent, child) {
     return parent.removeChild(child);
 }
 
+function balanceIncome(numString) {
+    incomeVal.innerHTML = Math.abs(parseFloat(incomeVal.innerHTML) - parseFloat(numString)).toFixed(2);
+}
+
+function balanceExpense(numString) {
+    expenseVal.innerHTML = Math.abs(parseFloat(expenseVal.innerHTML) - parseFloat(numString)).toFixed(2);
+}
+
 function addToHistory(sign, color) {
     let newElement = document.createElement('div');
     newElement.className = 'indiv-history-value-container';
@@ -86,7 +94,12 @@ function addToHistory(sign, color) {
     delBtn.addEventListener('click', (e) => {
         e.preventDefault();
         historyContainer.removeChild(newElement);
-        console.log('This is working');
+        if (sign === '-') {
+            balanceExpense(pHistory.innerHTML.substring(2));
+        } else {
+            balanceIncome(pHistory.innerHTML.substring(2));
+        }
+        
     });
 
     //Problems 8/15/2020: Deletion of all newElement Children on click/ Delete button only works for the first one/ If you delete everything, and try to add again, it won't let you.. - Solved -- Apparently, you can't use innerHTML along with document.createElement or else the addEventListener will not work...
@@ -111,7 +124,8 @@ form.addEventListener('submit', (event) => {
 3. Add +$____ to positive integers, and -$_____ to negative integers -- DONE
 4. Add Updated Income / Expenses -- DONE;
 5. Add Updated Balances --DONE
-6. Add Ability To Remove History Transactions
+6. Add Ability To Remove History Transactions --DONE
+7. Update Balance, income, and expense upon deletion 
 7. Add persistence to local storage
 
 */
